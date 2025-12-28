fc7c508cb355a6f2e1f2a4b0d68f5828976b7d61
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2025-12-28 23:22:20 +0100

    test

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!!
       </h1>
     </main>
   );

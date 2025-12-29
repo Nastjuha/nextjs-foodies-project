0ee1811a8f81dd8073b1c28b267151702f7ebce0
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2025-12-29 11:39:27 +0100

    adding route structure

--- app/page.js
+++ app/page.js
@@ -1,9 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!!
       </h1>
+
+      <p>
+        <Link href="/meals">Meals Page</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Meals Share Page</Link>
+      </p>
+      <p>
+        <Link href="/community">Community Page</Link>
+      </p>
+      <p>
+        <Link href="/meals/spaghetti">Meals Slug Page (spaghetti)</Link>
+      </p>
     </main>
   );
 }

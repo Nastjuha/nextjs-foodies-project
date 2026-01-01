eb9f4ce4194af60ccb5ad940db4e22628a0ec1e3
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2026-01-01 18:33:30 +0100

    Using Client Components Efficiently

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,11 +1,18 @@
+// "use client"; // we want to add this as far down the component tree as possible
+// to convert to a client component only needed part
+
 import Link from "next/link";
 import Image from "next/image";
+// import { usePathname } from "next/navigation";
 
 import MainHeaderBackground from "./main-header-background";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+  // const path = usePathname(); // returns currently avtive path - path after the domain
+
   return (
     <>
       <MainHeaderBackground />
@@ -18,10 +25,26 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
+
+            {/* <Link
+                href="/meals"
+                className={
+                  path.startsWith("/meals") ? classes.active : undefined
+                } // startsWith -  if we-re on a meals or any nested page
+              >
+                Browse Meals
+              </Link> */}
+
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
+              {/* <Link
+                href="/community"
+                className={path === "/community" ? classes.active : undefined}
+              > exact match - we don-t have nested pages here
+                Foodies Community
+              </Link> */}
             </li>
           </ul>
         </nav>

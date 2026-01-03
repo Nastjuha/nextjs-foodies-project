5d254253e344008cbb02849afd636e617b91a29f
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2026-01-03 10:41:05 +0100

    Handling Errors

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+// because NextJS ensures we can catch any error, including that ones on client side, so after the pages were rendered on the server
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Failed to fetch meal data. Please try again later.</p>
+    </main>
+  );
+}

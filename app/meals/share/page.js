3c710bb4a53e3da175c293fc489880d5dd23428b
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2026-01-06 21:52:36 +0100

    Form Submission with Using Server Actions

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -3,6 +3,22 @@ import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    "use server";
+
+    //object 'formData' contains all the submitted data from input fields ->  to handle the submitted data -> extract them
+    const meal = {
+      title: formData.get("title"), // get the data from the input field with the name='title'
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"), // see ImagePicker component how we handle the image upload and get the image file -> property
+      creator: formData.get("name"),
+      email: formData.get("email"),
+    }; // form data object
+
+    console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +28,11 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        {/* value of action - server action f. Pattern that supported by Next.js and React that will ensure that when this form is 
+        submitted, Next.js will behind the scenes create a request and send it to this Next.js server, that serving the website,
+        so as the function shareMeal() gets triggered and we can handle the form submission there, but on the server. =>
+        that function will execute on the server, NOT in the client. That f. will automatically recieve formData that was submitted.  */}
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,7 +60,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>

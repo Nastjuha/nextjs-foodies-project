9721ef6abcecc6348e885985e19f68dac8ecb42c
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2026-01-07 17:22:55 +0100

    Storing Server Actions in Separate Files

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,24 +1,8 @@
-import classes from "./page.module.css";
-
 import ImagePicker from "@/components/meals/image-picker";
+import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server";
-
-    //object 'formData' contains all the submitted data from input fields ->  to handle the submitted data -> extract them
-    const meal = {
-      title: formData.get("title"), // get the data from the input field with the name='title'
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"), // see ImagePicker component how we handle the image upload and get the image file -> property
-      creator: formData.get("name"),
-      email: formData.get("email"),
-    }; // form data object
-
-    console.log(meal);
-  }
-
   return (
     <>
       <header className={classes.header}>
